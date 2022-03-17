15ec08cfbf5e9d68e899aa3125bdda1da814303e
Terrell Burton <lilmill64@yahoo.com>
2022-03-16 20:20:11 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDA9FhiPIQYr1IR_wIyXSISZfn_Wsb3gJ0",
+  authDomain: "codenames-98c02.firebaseapp.com",
+  databaseURL: "https://codenames-98c02-default-rtdb.firebaseio.com",
+  projectId: "codenames-98c02",
+  storageBucket: "codenames-98c02.appspot.com",
+  messagingSenderId: "202410102642",
+  appId: "1:202410102642:web:7f114b0cea758656e757eb"
+};
